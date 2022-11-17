0412c7f80f6f5148f84ec6dea25df6db0cc9aff6
Emel <ebu.pro@gmail.com>
2022-11-18 00:56:48 +0100

    add getErc721 function, add offset parameter

--- cogs/commands.py
+++ cogs/commands.py
@@ -20,8 +20,8 @@ class Commands(commands.Cog):
     - Return 10 transaction.
     """
     @commands.command()
-    async def getTrx(self, ctx: commands.Context, address: str, key=key, counter=0):    
-        endpoint = f'https://api.polygonscan.com/api?module=account&action=txlist&address={str(address)}&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey={str(key)}'
+    async def getTrx(self, ctx: commands.Context, address: str, offset: str, key=key, counter=0):    
+        endpoint = f'https://api.polygonscan.com/api?module=account&action=txlist&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(key)}'
         response = requests.get(endpoint)  
         data = json.loads(response.text)
         
@@ -47,12 +47,11 @@ class Commands(commands.Cog):
     Define getErc20() - return list of ERC-20 transactions, can be filtered by specific smart contract address. 
     """
     @commands.command()
-    async def getErc20(self, ctx: commands.Context, address: str, contract: str, key=key, counter=0):
+    async def getErc20(self, ctx: commands.Context, address: str, contract: str, offset: str, key=key, counter=0):
         if(contract == 'SAND'):
-            contract = '0xbbba073c31bf03b8acf7c28ef0738decf3695683'
-        else:
-            return 
-        endpoint = f'https://api.polygonscan.com/api?module=account&action=tokentx&contractaddress={str(contract)}&address={str(address)}&startblock=0&endblock=99999999&page=1&offset=5&sort=desc&apikey={str(key)}'
+            contract = '0xbbba073c31bf03b8acf7c28ef0738decf3695683' 
+        
+        endpoint = f'https://api.polygonscan.com/api?module=account&action=tokentx&contractaddress={str(contract)}&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(key)}'
         response = requests.get(endpoint)  
         data = json.loads(response.text)  
         for each in data['result']:
@@ -62,7 +61,24 @@ class Commands(commands.Cog):
             dt = datetime.fromtimestamp(ts)   
             string = '**Transaction #' + str(counter) + '**\nFrom: ' + str(each['from']) + ' \nTo: ' + str(each['to']) + '\nWhen: ' + str(dt) + '\nValue: ' + str(value) + '\n--------------------------------------------------------------------'
             await ctx.send(string) 
+    
+    """
+    Define getErc721() - return list of ERC-721 (NFT) transactions, can be filtered by specific smart contract address. 
+    """
+    @commands.command()
+    async def getErc721(self, ctx: commands.Context, address: str, contract: str, offset: str, key=key, counter=0):
+        if(contract == 'LAND'):
+            contract = '0x9d305a42A3975Ee4c1C57555BeD5919889DCE63F'
+
+        endpoint = f'https://api.polygonscan.com/api?module=account&action=tokennfttx&contractaddress={str(contract)}&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(key)}'
+        response = requests.get(endpoint)
+        data = json.loads(response.text)
+        for each in data['result']:
+            counter += 1 
+            ts = int(each['timeStamp'])
+            dt = datetime.fromtimestamp(ts)
+            string = '**Transaction #' + str(counter) + '**\nToken Name: ' + str(each['tokenName']) + '\nToken ID:' + str(each['tokenID']) + '\nFrom: ' + str(each['from']) + '\nTo: ' + str(each['to']) + '\nWhen' + str(dt)
+            await ctx.send(string)
 
 def setup(bot):
     bot.add_cog(Commands(bot))
-
