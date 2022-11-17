a4e5f7de785927ee7d075c1c651f6f0eadedb525
Emel <ebu.pro@gmail.com>
2022-11-17 20:40:31 +0100

    add on_command_error()

--- cogs/commands.py
+++ cogs/commands.py
@@ -11,7 +11,6 @@ intents.message_content = True
 key = str(APIKey)  
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("ps-"), intents=intents)
 
-
 class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -22,7 +21,7 @@ class Commands(commands.Cog):
     """
     @commands.command()
     async def getTrx(self, ctx: commands.Context, address: str, key=key, counter=0):    
-        endpoint = 'https://api.polygonscan.com/api?module=account&action=txlist&address=' + str(address) + '&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=' + str(key)
+        endpoint = f'https://api.polygonscan.com/api?module=account&action=txlist&address={str(address)}&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey={str(key)}'
         response = requests.get(endpoint)  
         data = json.loads(response.text)
         
@@ -30,7 +29,7 @@ class Commands(commands.Cog):
             counter += 1     
             ts = int(each['timeStamp'])  
             dt = datetime.fromtimestamp(ts)   
-            string = 'Transaction #' + str(counter) + '\nFrom: ' + str(each['from']) + ' \nTo: ' + str(each['to']) + '\nWhen: ' + str(dt) + '\n--------------------------------------------------------------------'
+            string = '**Transaction #' + str(counter) + '**\nFrom: ' + str(each['from']) + ' \nTo: ' + str(each['to']) + '\nWhen: ' + str(dt) + '\n--------------------------------------------------------------------'
             await ctx.send(string)
 
     """
@@ -42,7 +41,7 @@ class Commands(commands.Cog):
         response = requests.get(endpoint)  
         data = json.loads(response.text) 
         amount = float(data['result']) / ( 10 ** 18 ) # Convert WEI to MATIC
-        await ctx.send("Amount in MATIC: " + str(amount)) 
+        await ctx.send("Amount in **MATIC**: " + str(amount)) 
 
     """
     Define getErc20() - return list of ERC-20 transactions, can be filtered by specific smart contract address. 
@@ -61,7 +60,7 @@ class Commands(commands.Cog):
             value = int(each['value']) / 10 ** 18 # Convert WEI to SAND
             ts = int(each['timeStamp'])  
             dt = datetime.fromtimestamp(ts)   
-            string = 'Transaction #' + str(counter) + '\nFrom: ' + str(each['from']) + ' \nTo: ' + str(each['to']) + '\nWhen: ' + str(dt) + '\nValue: ' + str(value) + '\n--------------------------------------------------------------------'
+            string = '**Transaction #' + str(counter) + '**\nFrom: ' + str(each['from']) + ' \nTo: ' + str(each['to']) + '\nWhen: ' + str(dt) + '\nValue: ' + str(value) + '\n--------------------------------------------------------------------'
             await ctx.send(string) 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -26,6 +26,16 @@ async def on_ready():
     print("Welcome to the PolygonScan Tracker Bot!")
     print(f"Logged in as {bot.user} (ID: {bot.user.id})\n------------------------------------")
 
+# Display error on command
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("**Invalid command. Try using** `help` **to figure out commands!**")
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('**Please pass in all requirements.**')
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("**You dont have all the requirements or permissions for using this command :angry:**")
+
 for file in os.listdir('./cogs'):
     if file.endswith('.py') and file != '__init__.py':
         try:
