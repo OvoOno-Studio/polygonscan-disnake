790a010c405ef978ba0998c747ee6e1eccb2f2a7
Emel <ebu.pro@gmail.com>
2022-11-27 16:20:41 +0100

    fix token reveal, add mention user message, log the console

--- TODO.md
+++ TODO.md
@@ -2,8 +2,7 @@
 
 - [x] Create function to get Erc1155 Tokens
 - [x] Create Help command
-- [ ] Mention Author User of command
-- [ ] Optimize functions to prevent reveal tokens in /help command
-- [ ] Define Free and Premium version
+- [x] Mention Author User of command
+- [x] Optimize functions to prevent reveal tokens in /help command
 - [ ] Add option to sent DM ( Premium Version )
 - [ ] Limit the max offset ( Free Version )
--- cogs/commands.py
+++ cogs/commands.py
@@ -24,6 +24,8 @@ class Commands(commands.Cog):
         # endpoint = f'https://api.polygonscan.com/api?module=account&action=txlistinternal&txhash={str(hash)}&apikey={str(key)}'
         # response = requests.get(endpoint)
         # data = json.loads(response.text) 
+        author = ctx.author.mention
+        print(f"User - {author} trigger command getTrxHash for {hash}.")
         await ctx.send(f'https://polygonscan.com/tx/{str(hash)}')        
 
     """
@@ -36,7 +38,9 @@ class Commands(commands.Cog):
         endpoint = f'https://api.polygonscan.com/api?module=account&action=txlist&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(api_key)}'
         response = requests.get(endpoint)  
         data = json.loads(response.text)
-        
+        author = ctx.author.mention
+        print(f"User - {author} trigger command getTrx for {address} wallet address.")
+        await ctx.send(f"Listing last {offset} internal transactions for **{address}** - by {author}") 
         for each in data['result']:
             counter += 1     
             ts = int(each['timeStamp'])  
@@ -48,24 +52,32 @@ class Commands(commands.Cog):
     Define getBalance() - get amount in WEI for single address. 
     """
     @commands.command()
-    async def getBalance(self, ctx: commands.Context, address: str, key=key): 
-        endpoint = f'https://api.polygonscan.com/api?module=account&action=balance&address={str(address)}&apikey={str(key)}'
+    async def getBalance(self, ctx: commands.Context, address: str): 
+        api_key = self.key
+        endpoint = f'https://api.polygonscan.com/api?module=account&action=balance&address={str(address)}&apikey={str(api_key)}'
         response = requests.get(endpoint)  
         data = json.loads(response.text) 
         amount = float(data['result']) / ( 10 ** 18 ) # Convert WEI to MATIC
+        author = ctx.author.mention
+        print(f"User - {author} trigger command getBalance for {address} wallet address.")
+        await ctx.send(f"Listing MATIC balance for **{address}** - by {author}") 
         await ctx.send("Amount in **MATIC**: " + str(amount)) 
 
     """
     Define getErc20() - return list of ERC-20 transactions, can be filtered by specific smart contract address. 
     """
     @commands.command()
-    async def getErc20(self, ctx: commands.Context, address: str, contract: str, offset: str, key=key, counter=0):
+    async def getErc20(self, ctx: commands.Context, address: str, contract: str, offset: str, counter=0):
+        api_key = self.key   
         if(contract == 'SAND'):
             contract = '0xbbba073c31bf03b8acf7c28ef0738decf3695683' 
         
-        endpoint = f'https://api.polygonscan.com/api?module=account&action=tokentx&contractaddress={str(contract)}&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(key)}'
+        endpoint = f'https://api.polygonscan.com/api?module=account&action=tokentx&contractaddress={str(contract)}&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(api_key)}'
         response = requests.get(endpoint)  
-        data = json.loads(response.text)  
+        data = json.loads(response.text) 
+        author = ctx.author.mention
+        print(f"User - {author} trigger command getErc20 for {address} wallet address.")
+        await ctx.send(f"Listing last {offset} ERC-20 transactions for **{address}** - by {author}") 
         for each in data['result']:
             counter += 1     
             value = int(each['value']) / 10 ** 18 # Convert WEI to SAND
@@ -78,13 +90,17 @@ class Commands(commands.Cog):
     Define getErc721() - return list of ERC-721 (NFT) transactions, can be filtered by specific smart contract address. 
     """
     @commands.command()
-    async def getErc721(self, ctx: commands.Context, address: str, contract: str, offset: str, key=key, counter=0):
+    async def getErc721(self, ctx: commands.Context, address: str, contract: str, offset: str, counter=0):
+        api_key = self.key
         if(contract == 'LAND'):
             contract = '0x9d305a42A3975Ee4c1C57555BeD5919889DCE63F'
 
-        endpoint = f'https://api.polygonscan.com/api?module=account&action=tokennfttx&contractaddress={str(contract)}&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(key)}'
+        endpoint = f'https://api.polygonscan.com/api?module=account&action=tokennfttx&contractaddress={str(contract)}&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(api_key)}'
         response = requests.get(endpoint)
         data = json.loads(response.text)
+        author = ctx.author.mention
+        print(f"User - {author} trigger command getErc721 for {address} wallet address.")
+        await ctx.send(f"Listing last {offset} ERC-20 transactions for **{address}** - by {author}") 
         for each in data['result']:
             counter += 1 
             ts = int(each['timeStamp'])
@@ -96,10 +112,14 @@ class Commands(commands.Cog):
     Define getErc1155() - return list of ERC-721 (NFT) transactions, can be filtered by specific smart contract address. 
     """
     @commands.command()
-    async def getErc1155(self, ctx: commands.Context, address: str, contract: str, offset: str, key=key, counter=0):
-        endpoint = f'https://api.etherscan.io/api?module=account&action=token1155tx&contractaddress={str(contract)}&address={str(address)}&page=1&{str(offset)}=100&startblock=0&endblock=99999999&sort=asc&apikey={str(key)}'
+    async def getErc1155(self, ctx: commands.Context, address: str, contract: str, offset: str, counter=0):
+        api_key = self.key
+        endpoint = f'https://api.etherscan.io/api?module=account&action=token1155tx&contractaddress={str(contract)}&address={str(address)}&page=1&{str(offset)}=100&startblock=0&endblock=99999999&sort=asc&apikey={str(api_key)}'
         response = requests.get(endpoint)
         data = json.loads(response.text)
+        author = ctx.author.mention
+        print(f"User - {author} trigger command getErc721 for {address} wallet address.")
+        await ctx.send(f"Listing last {offset} ERC-1155 transactions for **{address}** - by {author}") 
         for each in data['result']:
             counter += 1 
             ts = int(each['timeStamp'])
