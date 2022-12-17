a1c0b810d93688e75893d90aef40fec39acf8add
Emel <ebu.pro@gmail.com>
2022-12-18 00:35:13 +0100

    optimize functions to sent DM instead messages in channel

--- cogs/commands.py
+++ cogs/commands.py
@@ -20,48 +20,93 @@ class Commands(commands.Cog):
     Define getTrxHash() - return a link to for specific transaction hash.
     """
     @commands.command()
-    async def getTrxHash(self, ctx: commands.Context, hash: str):
-        # endpoint = f'https://api.polygonscan.com/api?module=account&action=txlistinternal&txhash={str(hash)}&apikey={str(key)}'
-        # response = requests.get(endpoint)
-        # data = json.loads(response.text) 
-        author = ctx.author.mention
-        print(f"User - {author} trigger command getTrxHash for {hash}.")
-        await ctx.send(f'https://polygonscan.com/tx/{str(hash)}')        
-
+    async def getTrxHash(self, ctx: commands.Context, hash: str): 
+        author = ctx.author.mention 
+        # await ctx.send(f'https://polygonscan.com/tx/{str(hash)}') 
+        print(f"User - {author} trigger command getTrxHash for {hash} transaction.")
+        await ctx.send(f"Generating link for transaction with {str(hash)} - sent DM to {author}")
+        embed = disnake.Embed(
+            title="Get transaction by hash ID",
+            description="Return a link to for specific transaction hash,",
+            color=0x9C84EF,
+            timestamp=datetime.now()
+        ) 
+        embed.add_field(
+            name="Link of Transaction Hash:",
+            value=f'https://polygonscan.com/tx/{str(hash)}',
+            inline=False
+        )   
+        embed.set_footer(
+            text=f"Requested by {ctx.author}"
+        )        
+        await ctx.author.send(embed=embed)
     """
     Define getTrx() - function that will fetch all normal transactions for specific address. 
     """
     @commands.command()
     async def getTrx(self, ctx: commands.Context, address: str, offset: str):
-        counter = 0
+        author = ctx.author.mention
         api_key = self.key    
+        counter = 0
         endpoint = f'https://api.polygonscan.com/api?module=account&action=txlist&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(api_key)}'
         response = requests.get(endpoint)  
         data = json.loads(response.text)
         author = ctx.author.mention
         print(f"User - {author} trigger command getTrx for {address} wallet address.")
-        await ctx.send(f"Listing last {offset} internal transactions for **{address}** - by {author}") 
+        await ctx.send(f"Listing last {offset} internal transactions for **{address}** - sent DM to {author}")
+        embed = disnake.Embed(
+            title=f"{str(offset)} transactions of {str(address)}",
+            description="Return list of normal transaction",
+            color=0x9C84EF,
+            timestamp=datetime.now()
+        )   
+        
         for each in data['result']:
             counter += 1     
             ts = int(each['timeStamp'])  
             dt = datetime.fromtimestamp(ts)   
-            string = '**Transaction #' + str(counter) + '**\nTransaction Hash: ' + str(each['hash']) + '\nFrom: ' + str(each['from']) + ' \nTo: ' + str(each['to']) + '\nWhen: ' + str(dt) + '\n--------------------------------------------------------------------'
-            await ctx.send(string)
+            # string = '**Transaction #' + str(counter) + '**\nTransaction Hash: ' + str(each['hash']) + '\nFrom: ' + str(each['from']) + ' \nTo: ' + str(each['to']) + '\nWhen: ' + str(dt) + '\n--------------------------------------------------------------------'
+            embed.add_field(
+                name=f"Transaction #{str(counter)}",
+                value=f'\nTransaction Hash: {str(each["hash"])} \nFrom: {str(each["from"])} \nTo: {str(each["to"])} \nWhen: {str(dt)}',
+                inline=False 
+            )
 
+        embed.set_footer(
+            text=f"Requested by {ctx.author}"
+        )  
+
+        await ctx.author.send(embed=embed)    
+    
     """
     Define getBalance() - get amount in WEI for single address. 
     """
     @commands.command()
     async def getBalance(self, ctx: commands.Context, address: str): 
         api_key = self.key
+        author = ctx.author.mention
         endpoint = f'https://api.polygonscan.com/api?module=account&action=balance&address={str(address)}&apikey={str(api_key)}'
         response = requests.get(endpoint)  
         data = json.loads(response.text) 
-        amount = float(data['result']) / ( 10 ** 18 ) # Convert WEI to MATIC
-        author = ctx.author.mention
+        amount = float(data['result']) / ( 10 ** 18 ) # Convert WEI to MATIC 
         print(f"User - {author} trigger command getBalance for {address} wallet address.")
-        await ctx.send(f"Listing MATIC balance for **{address}** - by {author}") 
-        await ctx.send("Amount in **MATIC**: " + str(amount)) 
+        await ctx.send(f"Sending MATIC balance for **{address}** - sent DM to {author}") 
+        embed = disnake.Embed(
+            title=f"Get balance for {str(address)}",
+            description="Return amount in WEI (converted) for single address",
+            color=0x9C84EF,
+            timestamp=datetime.now()
+        ) 
+        embed.add_field(
+            name="Amount in MATIC:",
+            value=amount,
+            inline=False
+        )   
+        embed.set_footer(
+            text=f"Requested by {ctx.author}"
+        ) 
+
+        await ctx.author.send(embed=embed) 
 
     """
     Define getErc20() - return list of ERC-20 transactions, can be filtered by specific smart contract address. 
@@ -100,7 +145,7 @@ class Commands(commands.Cog):
         data = json.loads(response.text)
         author = ctx.author.mention
         print(f"User - {author} trigger command getErc721 for {address} wallet address.")
-        await ctx.send(f"Listing last {offset} ERC-20 transactions for **{address}** - by {author}") 
+        await ctx.send(f"Listing last {offset} ERC-20 transactions for **{address}** - sent DM to {author}") 
         for each in data['result']:
             counter += 1 
             ts = int(each['timeStamp'])
