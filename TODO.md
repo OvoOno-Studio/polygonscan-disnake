a05a6b2a500e1f74c2cf99560f8d6c0acb3980f9
Emel <ebu.pro@gmail.com>
2022-12-26 19:52:39 +0100

    added checkTrx function, optimized others

--- TODO.md
+++ TODO.md
@@ -6,6 +6,7 @@
 - [x] Optimize functions to prevent reveal tokens in /help command
 - [x] Add option to sent DM ( Premium Version )
 - [x] Limit the max offset ( Free Version )
-- [ ] Optimize getErc20, getErc721, getErc1155 to send DM's to author and set max offset
-- [ ] Add function to check status by transaction hash
+- [x] Optimize getErc20, getErc721, getErc1155 to send DM's to author and set max offset
+- [x] Add function to check status by transaction hash
 - [ ] Update content of messages with emojies, font styling etc
+- [ ] Add message if there is no result
--- cogs/commands.py
+++ cogs/commands.py
@@ -15,6 +15,46 @@ class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.key = str(APIKey)
+    
+    """
+    Define checkTrx() - check status of transaction by hash.
+    """
+    @commands.command()
+    async def checkTrx(self, ctx: commands.Context, hash: str): 
+        author = ctx.author.mention
+        api_key = self.key
+        counter = 0
+        endpoint = f'https://api.polygonscan.com/api?module=transaction&action=gettxreceiptstatus&txhash={str(hash)}&apikey={str(api_key)}'
+        response = requests.get(endpoint)  
+        data = json.loads(response.text)
+        print(f"User - {author} trigger command checkTrx for {hash} transaction.")
+        await ctx.send(f"Return status of transaction with {str(hash)} - sent DM to {author}")
+        embed = disnake.Embed(
+            title=f"Status of transaction with hash {str(hash)}",
+            description="Return list of ERC-20 transaction.",
+            color=0x9C84EF,
+            timestamp=datetime.now()
+        )
+
+        for each in data['result']:
+            counter += 1 
+            status = ''     
+            if(each['status'] == 1):
+                status = 'Successful'
+            if(each['status'] == 0):
+                status = "Failed"
+
+            embed.add_field(
+                name=f"Status transaction: ",
+                value=f'\n {str(status)}',
+                inline=False 
+            )
+        
+        embed.set_footer(
+            text=f"Requested by {ctx.author}"
+        )  
+
+        await ctx.author.send(embed=embed) 
 
     """
     Define getTrxHash() - return a link to for specific transaction hash.
@@ -40,6 +80,7 @@ class Commands(commands.Cog):
             text=f"Requested by {ctx.author}"
         )        
         await ctx.author.send(embed=embed)
+
     """
     Define getTrx() - function that will fetch all normal transactions for specific address. 
     """
@@ -160,41 +201,77 @@ class Commands(commands.Cog):
     """
     @commands.command()
     async def getErc721(self, ctx: commands.Context, address: str, contract: str, offset: str, counter=0):
-        api_key = self.key
+        if(int(offset) > 25):
+            await ctx.send(f"Maximum offset must be lower then 25! Aborting the command.")
+            return
+
         if(contract == 'LAND'):
             contract = '0x9d305a42A3975Ee4c1C57555BeD5919889DCE63F'
 
+        api_key = self.key
         endpoint = f'https://api.polygonscan.com/api?module=account&action=tokennfttx&contractaddress={str(contract)}&address={str(address)}&startblock=0&endblock=99999999&page=1&offset={str(offset)}&sort=desc&apikey={str(api_key)}'
         response = requests.get(endpoint)
         data = json.loads(response.text)
         author = ctx.author.mention
         print(f"User - {author} trigger command getErc721 for {address} wallet address.")
         await ctx.send(f"Listing last {offset} ERC-20 transactions for **{address}** - sent DM to {author}") 
+        embed = disnake.Embed(
+            title=f"{str(offset)} ERC-721 transactions of {str(address)}",
+            description="Return list of ERC-721 transaction.",
+            color=0x9C84EF,
+            timestamp=datetime.now()
+        ) 
         for each in data['result']:
             counter += 1 
             ts = int(each['timeStamp'])
             dt = datetime.fromtimestamp(ts)
-            string = '**Transaction #' + str(counter) + '**\nToken Name: ' + str(each['tokenName']) + '\nToken ID:' + str(each['tokenID']) + '\nFrom: ' + str(each['from']) + '\nTo: ' + str(each['to']) + '\nWhen' + str(dt)
-            await ctx.send(string)
+            #string = '**Transaction #' + str(counter) + '**\nToken Name: ' + str(each['tokenName']) + '\nToken ID:' + str(each['tokenID']) + '\nFrom: ' + str(each['from']) + '\nTo: ' + str(each['to']) + '\nWhen' + str(dt)
+            embed.add_field(
+                name=f"Transaction #{str(counter)}",
+                value=f'\nTransaction Name: {str(each["tokenName"])} \nToken ID: {str(each["tokenID"])} \nFrom: {str(each["from"])} \nTo: {str(each["to"])} \nWhen: {str(dt)}',
+                inline=False 
+            )
+
+        embed.set_footer(
+            text=f"Requested by {ctx.author}"
+        ) 
+        
+        await ctx.send(embed=embed)
 
     """
     Define getErc1155() - return list of ERC-721 (NFT) transactions, can be filtered by specific smart contract address. 
     """
     @commands.command()
     async def getErc1155(self, ctx: commands.Context, address: str, contract: str, offset: str, counter=0):
+        if(int(offset) > 25):
+            await ctx.send(f"Maximum offset must be lower then 25! Aborting the command.")
+            return
+        
         api_key = self.key
         endpoint = f'https://api.etherscan.io/api?module=account&action=token1155tx&contractaddress={str(contract)}&address={str(address)}&page=1&{str(offset)}=100&startblock=0&endblock=99999999&sort=asc&apikey={str(api_key)}'
         response = requests.get(endpoint)
         data = json.loads(response.text)
         author = ctx.author.mention
         print(f"User - {author} trigger command getErc721 for {address} wallet address.")
-        await ctx.send(f"Listing last {offset} ERC-1155 transactions for **{address}** - by {author}") 
+        await ctx.send(f"Listing last {offset} ERC-1155 transactions for **{address}** - sent DM to {author}") 
+        embed = disnake.Embed(
+            title=f"{str(offset)} ERC-1155 transactions of {str(address)}",
+            description="Return list of ERC-1155 transaction.",
+            color=0x9C84EF,
+            timestamp=datetime.now()
+        ) 
         for each in data['result']:
             counter += 1 
             ts = int(each['timeStamp'])
             dt = datetime.fromtimestamp(ts)
-            string = '**Transaction #' + str(counter) + '**\nToken Name: ' + str(each['tokenName']) + '\nToken ID:' + str(each['tokenID']) + '\nFrom: ' + str(each['from']) + '\nTo: ' + str(each['to']) + '\nWhen' + str(dt)
-            await ctx.send(string)
+            #string = '**Transaction #' + str(counter) + '**\nToken Name: ' + str(each['tokenName']) + '\nToken ID:' + str(each['tokenID']) + '\nFrom: ' + str(each['from']) + '\nTo: ' + str(each['to']) + '\nWhen' + str(dt)
+            embed.add_field(
+                name=f"Transaction #{str(counter)}",
+                value=f'\nTransaction Name: {str(each["tokenName"])} \nToken ID: {str(each["tokenID"])} \nFrom: {str(each["from"])} \nTo: {str(each["to"])} \nWhen: {str(dt)}',
+                inline=False 
+            )
+            
+        await ctx.send(embed=embed)
     
 def setup(bot):
     bot.add_cog(Commands(bot))
