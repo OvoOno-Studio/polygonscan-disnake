a2ba6095653395642633dbb74e115d3b668955d9
unknown <ebu.pro@gmail.com>
2023-06-03 20:24:58 +0200

    test

--- cogs/events.py
+++ cogs/events.py
@@ -8,6 +8,7 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         # Initialize the server's configuration in the database
+        print('testing...')
         set_wallet_address(guild.id, 'default_wallet_address')
         set_transaction_channel(guild.id, 'default_transaction_channel')
         set_price_alert_channel(guild.id, 'default_price_alert_channel')
