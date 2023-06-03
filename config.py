f688edfadb1cc190132afa5fcba45b2fd4637265
unknown <ebu.pro@gmail.com>
2023-06-03 05:37:06 +0200

    replit db connection

--- config.py
+++ config.py
@@ -1,32 +1,33 @@
 # config.py
 from dotenv import load_dotenv
+from replit import db
 import os
 import json
 
 CONFIG_FILE = "config.json"
 
 def _load_config():
-    with open(CONFIG_FILE, "r") as f:
-        return json.load(f)
+  with open(CONFIG_FILE, "r") as f:
+    return json.load(f)
 
 def _save_config(config):
-    with open(CONFIG_FILE, "w") as f:
-        json.dump(config, f, indent=4)
+  with open(CONFIG_FILE, "w") as f:
+    json.dump(config, f, indent=4)
 
 def set_transaction_channel(channel_id):
-    config = _load_config()
-    config["transaction_channel_id"] = channel_id
-    _save_config(config)
+  config = _load_config()
+  config["transaction_channel_id"] = channel_id
+  _save_config(config)
 
 def set_price_alert_channel(channel_id):
-    config = _load_config()
-    config["price_alert_channel_id"] = channel_id
-    _save_config(config)
+  config = _load_config()
+  config["price_alert_channel_id"] = channel_id
+  _save_config(config)
 
 def set_wallet_address(wallet_address):
-    config = _load_config()
-    config["wallet_address"] = wallet_address
-    _save_config(config)
+  config = _load_config()
+  config["wallet_address"] = wallet_address
+  _save_config(config)
 
 config = _load_config()
 
@@ -38,6 +39,6 @@ DiscordToken = os.getenv('DISCORD_TOKEN')
 PublicKey = os.getenv('PUBLIC_KEY')
 APIKey = os.getenv('API_KEY')
 API2Key = os.getenv('API2_KEY')
-transaction_channel_id = config["transaction_channel_id"]
-price_alert_channel_id = config["price_alert_channel_id"]
-wallet_address = config["wallet_address"]
+transaction_channel_id = db['GuildConfig']['transaction_channel_id']
+price_alert_channel_id = db['GuildConfig']["price_alert_channel_id"]
+wallet_address = db['GuildConfig']["wallet_address"]  
