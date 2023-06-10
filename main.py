80337774e9cf785af2244ceda83efb299ef08382
unknown <ebu.pro@gmail.com>
2023-06-11 00:01:45 +0200

    test

--- main.py
+++ main.py
@@ -10,31 +10,7 @@ from disnake.ext import commands
 from config import DiscordToken
 import os, traceback
 
-class BaseCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("Welcome to the PolygonScan Tracker Bot!")
-        print(f"Logged in as {self.bot.user} (ID: {self.bot.user.id})\n--------------------------------------------------------------------")
-
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandNotFound):
-            await ctx.send("**Invalid command. Try using** `help` **to figure out commands!**")
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send('**Please pass in all requirements.**')
-        elif isinstance(error, commands.MissingPermissions):
-            await ctx.send("**You dont have all the requirements or permissions for using this command :angry:**")
-
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send (f"📶 {round(self.bot.latency * 1000)}ms")
-
-    @commands.command()
-    async def donate(self, ctx):
-        await ctx.send (f"To access all features from PolygonScan Scrapper Bot and OvoOno Studio in globally, you can donate with one-time PayPal payment on next link: https://upgrade.chat/ovoono-studio/p/ovodonator ")
+from cogs.base_cog import BaseCog
 
 class Bot(commands.Bot):
     def __init__(self):
