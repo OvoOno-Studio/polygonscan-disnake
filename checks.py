62f41853b838c11e6ed733fee524fd07098b9ae2
Emel <ebu.pro@gmail.com>
2023-06-11 21:24:23 +0200

    test

--- checks.py
+++ checks.py
@@ -1,12 +1,29 @@
 import disnake
+import aiohttp
+import json
 from disnake.ext import commands
 from replit import db
 
 # Define is donator to separate free and paied version
+async def fetch_donators():
+    headers = {
+        'accept': 'application/json',
+        'Authorization': 'Bearer your_token_here',
+        'client_id': 'your_client_id',
+        'client_secret': 'your_client_secret',
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.upgrade.chat/v1/users', headers=headers) as resp:
+            data = await resp.text()
+
+    return json.loads(data)
+
 def is_donator():
     async def predicate(ctx):
-        donator_role = disnake.utils.get(ctx.guild.roles, name="OvoDonator")
-        if donator_role in ctx.author.roles:
+        donators = await fetch_donators()
+        donator_ids = [donator['discord_id'] for donator in donators['data']]
+        if str(ctx.author.id) in donator_ids:
             return True
         else:
             raise commands.CheckFailure("You need to be a Donator to use this bot's commands.")
