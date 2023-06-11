d7f45fd8b55fffdd464a46f7985993c40ec8e542
unknown <ebu.pro@gmail.com>
2023-06-11 22:59:08 +0200

    error handling

--- checks.py
+++ checks.py
@@ -15,19 +15,29 @@ async def fetch_donators():
     }
 
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.upgrade.chat/v1/users', headers=headers) as resp:
-            data = await resp.text()
-
-    return json.loads(data)
+        try:
+            async with session.get('https://api.upgrade.chat/v1/users', headers=headers) as resp:
+                if resp.status == 200:
+                    data = await resp.text()
+                    return json.loads(data)
+                else:
+                    print(f"Failed to fetch donators. HTTP status code: {resp.status}")
+                    return None
+        except Exception as e:
+            print(f"Failed to fetch donators due to error: {e}")
+            return None
 
 def is_donator():
     async def predicate(ctx):
         donators = await fetch_donators()
-        donator_ids = [donator['discord_id'] for donator in donators['data']]
-        if str(ctx.author.id) in donator_ids:
-            return True
+        if donators is not None:
+            donator_ids = [donator['discord_id'] for donator in donators['data']]
+            if str(ctx.author.id) in donator_ids:
+                return True
+            else:
+                raise commands.CheckFailure("You need to be a Donator to use this bot's commands.")
         else:
-            raise commands.CheckFailure("You need to be a Donator to use this bot's commands.")
+            raise commands.CheckFailure("Failed to check donator status. Please try again later.")
     return commands.check(predicate)
 
 # Ensure if there is guild confiuration in database
