9fb862a1f5f8177755153ada1f0fffe283815c2e
unknown <ebu.pro@gmail.com>
2023-06-11 23:02:17 +0200

    error handling

--- checks.py
+++ checks.py
@@ -14,31 +14,36 @@ async def fetch_donators():
         'client_secret': str(config.Secret),
     }
 
+    # Print headers for debugging (remove or comment this out in production!)
+    print(f"Headers: {headers}")
+
     async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get('https://api.upgrade.chat/v1/users', headers=headers) as resp:
-                if resp.status == 200:
-                    data = await resp.text()
-                    return json.loads(data)
-                else:
-                    print(f"Failed to fetch donators. HTTP status code: {resp.status}")
-                    return None
-        except Exception as e:
-            print(f"Failed to fetch donators due to error: {e}")
-            return None
+        async with session.get('https://api.upgrade.chat/v1/users', headers=headers) as resp:
+            # Check the status of the response
+            if resp.status != 200:
+                # Print response text for debugging if status is not 200
+                print(f"Failed to fetch donators. HTTP status code: {resp.status}, response: {await resp.text()}")
+                return None
+
+            data = await resp.text()
+
+    return json.loads(data)
 
 def is_donator():
     async def predicate(ctx):
         donators = await fetch_donators()
-        if donators is not None:
-            donator_ids = [donator['discord_id'] for donator in donators['data']]
-            if str(ctx.author.id) in donator_ids:
-                return True
-            else:
-                raise commands.CheckFailure("You need to be a Donator to use this bot's commands.")
+        
+        # Add error handling for when fetch_donators fails
+        if not donators:
+            raise commands.CheckFailure("Failed to verify donator status due to an internal error.")
+        
+        donator_ids = [donator['discord_id'] for donator in donators['data']]
+        if str(ctx.author.id) in donator_ids:
+            return True
         else:
-            raise commands.CheckFailure("Failed to check donator status. Please try again later.")
+            raise commands.CheckFailure("You need to be a Donator to use this bot's commands.")
     return commands.check(predicate)
+
 
 # Ensure if there is guild confiuration in database
 def ensure_server_config(server_id):
