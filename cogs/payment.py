4bb091d06cabf7fab2fe1d779f6852e55baca7b0
unknown <ebu.pro@gmail.com>
2023-09-09 20:04:01 +0200

    add slash commands

--- cogs/payment.py
+++ cogs/payment.py
@@ -10,18 +10,17 @@ class Payment(commands.Cog):
         self.payment_wallet = '0x48D0eEE30ec5BF5C7E167036a8c7f3e4820c19fa'
         self.api = 'https://api.etherscan.io/api'
 
-    @commands.command(name='upgrade_version')
+    @commands.slash_command(name='upgrade_version')
     async def upgrade_version(self, ctx): 
-        message = f"Please send exactly 0.088 ETH to the address {self.payment_wallet} and let me know once done by typing `ps-confirm_payment <your_wallet>`."
-        await ctx.send(message)
+        message = f"Please send exactly 0.088 ETH to the address {self.payment_wallet} and let me know once done by typing `/confirm_payment <your_wallet>`."
+        await ctx.respond(content=message)
 
-    @commands.command(name='confirm_payment')
+    @commands.slash_command(name='confirm_payment')
     async def confirm_payment(self, ctx, wallet_from: str):
         wallet = self.payment_wallet
         api = self.api
         amount = 0.088
 
-        # Fetch the latest transactions for the address from Etherscan
         params = {
             "module": "account",
             "action": "txlist",
@@ -32,21 +31,24 @@ class Payment(commands.Cog):
             "apikey": API2Key
         }
 
-        response = requests.get(api, params=params)
+        try:
+            response = requests.get(api, params=params)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            await ctx.respond(content=f"Error fetching transaction details: {e}")
+            return
+
         data = response.json()
 
         if not data.get("result"):
-            await ctx.send("I couldn't verify the payment at this time. Please try again later.")
+            await ctx.respond(content="I couldn't verify the payment at this time. Please try again later.")
             return
 
         for tx in data["result"]:
-            # Verify transaction: from the user's specified address, correct amount, and to your address
-            if tx["to"] == wallet and tx["from"] == wallet_from.lower() and float(tx["value"]) == amount * 10**18:  # Ethereum has 18 decimal places
-                # Add user to donators.json
+            if tx["to"] == wallet and tx["from"] == wallet_from.lower() and float(tx["value"]) == amount * 10**18:
                 with open("donators.json", "r+") as file:
                     donators = json.load(file)
-                    
-                    # Check if user is already in the donators list
+
                     if not any(d['user_id'] == ctx.author.id for d in donators):
                         new_donator = {
                             "user_id": ctx.author.id,
@@ -56,9 +58,10 @@ class Payment(commands.Cog):
                         file.seek(0)
                         json.dump(donators, file)
 
-                await ctx.send("Payment verified! You have been upgraded to the premium version.")
+                await ctx.respond(content="Payment verified! You have been upgraded to the premium version.")
                 return
-        await ctx.send(f"Payment not found from wallet {wallet_from}. Ensure you've sent the correct amount to the right address.")  
+
+        await ctx.respond(content=f"Payment not found from wallet {wallet_from}. Ensure you've sent the correct amount to the right address.")
 
 def setup(bot):
     bot.add_cog(Payment(bot))
