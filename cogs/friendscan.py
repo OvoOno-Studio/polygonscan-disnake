76078bab86111edfd45aa0cac378c498fa868626
unknown <ebu.pro@gmail.com>
2023-10-17 05:48:10 +0200

    test

--- cogs/friendscan.py
+++ cogs/friendscan.py
@@ -17,6 +17,7 @@ class Friend(commands.Cog):
         self.friend_api = 'https://prod-api.kosetto.com'
         self.w3 = Web3(Web3.HTTPProvider('https://base-mainnet.g.alchemy.com/v2/8XQtglDUSx3Sp7MuWwhk3K1X9x2vrhJo'))
         self.wallet_address = '0xCF205808Ed36593aa40a44F10c7f7C2F67d4A4d4' 
+        self.guild_data = {}
         self.bot.loop.create_task(self.check_transactions())
         self.bot.loop.create_task(self.keys_alerts())
      
@@ -70,6 +71,7 @@ class Friend(commands.Cog):
                     print(f"Error sending message: {e}")
                 
     async def keys_alerts(self): 
+        print("keys_alerts function is running.")  # Add this line
         await self.bot.wait_until_ready()
         while not self.bot.is_closed():
             for guild in self.bot.guilds:
